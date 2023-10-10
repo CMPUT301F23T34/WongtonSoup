4b52a071eaa04e448c3aa4628efeb0b199340837
Liam Houston <liamdh23@gmail.com>
2023-10-10 14:41:29 -0600

    added trapezoid class

---
+++ app/src/main/java/com/example/wongtonsoup/Trapezoid.java
@@ -0,0 +1,15 @@
+package com.example.wongtonsoup;
+
+public class Trapezoid extends Shape {
+
+    protected int x;
+    protected int y;
+    protected int angle1;
+    protected int angle2;
+
+
+    public Trapezoid(int x, int y, int angle1, int angle2) {
+        super(x, y);
+    }
+
+}
