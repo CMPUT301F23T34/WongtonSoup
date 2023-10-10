d6be778b861ebc5775735b5e866a00f53b1d7b01
Liam Houston <liamdh23@gmail.com>
2023-10-10 12:07:39 -0600

    restored Matt's Shape interface (my bad)

---
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -0,0 +1,12 @@
+package com.example.wongtonsoup;
+public abstract class Shape {
+
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
