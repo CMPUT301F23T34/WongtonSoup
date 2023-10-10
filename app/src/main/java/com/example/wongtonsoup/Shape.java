7b8d4d8cb4799c6dff8f941b7f65ef733f4201db
Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>
2023-10-10 15:42:29 -0600

    gg for github (#42)
    * Star Model class
    
    * Added blue color
    
    * Pulling :) (#30)
    
    * Create Sqaure.java (#24)
    
    * Star Model class (#26)
    
    * add (#28)
    
    Co-authored-by: hoppee21 <yihui4@ualberta.ca>
    
    * added triangle class that extends shape (#27)
    
    * relocate
    
    * Update Shape.java (step 3)
    
    Did step 3. Added (String color = "$PUT_YOUR_CHOICE_OF_COLOR";)
    
    * Making conflicting pull request (#31)
    
    * added triangle class that extends shape
    
    * added colour to shape class
    
    ---------
    
    Co-authored-by: Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>
    
    * last pull request didn't merge with master (#32)
    
    * added trapezoid class
    
    * added init for angle vars
    
    * pull request not appearing on main for trapezoid
    
    ---------
    
    Co-authored-by: RylannAnd <100882433+RylannAnd@users.noreply.github.com>
    Co-authored-by: Liam Houston <105176614+liamhouston@users.noreply.github.com>
    Co-authored-by: hoppee21 <yihui4@ualberta.ca>
    Co-authored-by: Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>
    
    * changed to turquoise
    
    * changed color
    
    ---------
    
    Co-authored-by: RylannAnd <100882433+RylannAnd@users.noreply.github.com>
    Co-authored-by: Liam Houston <105176614+liamhouston@users.noreply.github.com>
    Co-authored-by: hoppee21 <yihui4@ualberta.ca>
    Co-authored-by: Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>

--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -8,6 +8,8 @@ public abstract class Shape {
 
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
+    String color = "teal";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
