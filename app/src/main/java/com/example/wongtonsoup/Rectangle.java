7d42980bfdc14980f1415932b1e7d7b44b0b0241
Matthew Jacobs <mjacobs@ualberta.ca>
2023-10-10 15:38:18 -0600

    changed to turquoise

--- app/src/main/java/com/example/wongtonsoup/Rectangle.java
+++ app/src/main/java/com/example/wongtonsoup/Rectangle.java
@@ -1,6 +1,8 @@
 package com.example.wongtonsoup;
 
 public class Rectangle extends Shape{
+    private int x;
+    private int y;
     public Rectangle(int x, int y) {
         super(x, y);
     }
--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -3,9 +3,8 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
-    String color = "blue";
 
-    String color = "blue";
+    String color = "turquoise";
 
     public Shape(int x, int y) {
         this.x = x;
