20fdad3dbca0e8bc61f8ce33743f5f0ab012c93a
RylannAnd <100882433+RylannAnd@users.noreply.github.com>
2023-10-10 15:51:09 -0600

    Update Shape.java

--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -3,7 +3,7 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
-    String color = "blue";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y) {
         this.x = x;
