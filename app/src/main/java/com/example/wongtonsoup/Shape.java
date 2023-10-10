7a985b02d5009258bc87a8c26a3bb6e2a359fa86
Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>
2023-10-10 14:58:13 -0600

    Pulling :) (#30)
    * Create Sqaure.java (#24)
    
    * Star Model class (#26)
    
    * add (#28)
    
    Co-authored-by: hoppee21 <yihui4@ualberta.ca>
    
    * added triangle class that extends shape (#27)
    
    * relocate
    
    * Update Shape.java (step 3)
    
    Did step 3. Added (String color = "$PUT_YOUR_CHOICE_OF_COLOR";)
    
    * Making conflicting pull request (#31)
    
    * added triangle class that extends shape
    
    * added colour to shape class
    
    ---------
    
    Co-authored-by: Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>
    
    * last pull request didn't merge with master (#32)
    
    * added trapezoid class
    
    * added init for angle vars
    
    * pull request not appearing on main for trapezoid
    
    ---------
    
    Co-authored-by: RylannAnd <100882433+RylannAnd@users.noreply.github.com>
    Co-authored-by: Liam Houston <105176614+liamhouston@users.noreply.github.com>
    Co-authored-by: hoppee21 <yihui4@ualberta.ca>
    Co-authored-by: Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>

--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -4,9 +4,13 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
+    String color = "blue";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+
     }
+
 
 }
