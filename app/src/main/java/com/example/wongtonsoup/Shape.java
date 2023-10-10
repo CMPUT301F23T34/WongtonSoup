fe574d6e60c9f07a6f552ab0d79238af695dfbef
Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>
2023-10-10 14:57:18 -0600

    Making conflicting pull request (#31)
    * added triangle class that extends shape
    
    * added colour to shape class
    
    ---------
    
    Co-authored-by: Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>

--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -3,11 +3,13 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+
+    String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+
     }
 
 
