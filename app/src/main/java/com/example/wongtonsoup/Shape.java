aa14a325e15ef3981252ba640e96aa600b6c625a
Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>
2023-10-10 14:45:37 -0600

    added colour to shape class

--- app/src/main/java/com/example/wongtonsoup/Shape.java
+++ app/src/main/java/com/example/wongtonsoup/Shape.java
@@ -3,10 +3,12 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
+    String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+
     }
 
 
