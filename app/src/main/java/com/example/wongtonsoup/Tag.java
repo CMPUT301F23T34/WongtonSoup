bbe594d93b9556f96c472d0b2722782da88c2791
Liam Houston <liamdh23@gmail.com>
2023-10-30 18:58:06 -0600

    added tag tests

--- app/src/main/java/com/example/wongtonsoup/Tag.java
+++ app/src/main/java/com/example/wongtonsoup/Tag.java
@@ -17,12 +17,16 @@ public class Tag implements Comparable<Tag> {
     }
 
     /**
-     * Tags are compared by their names.
+     * Tags are compared by their names. Comparison is not case-sensitive ("aab" == "AaB").
      * @param t the tag to be compared.
      * @return
      */
     @Override
     public int compareTo(Tag t) {
-        return this.name.compareTo(t.name);
+        // assume we have tag objects tag1, tag2. tag1.name = 'first', tag2.name = 'last'.
+        // -- tag1.compareTo(tag2) will return positive
+        // -- tag1.compareTo(tag1) will return zero
+        // -- tag2.compareTo(tag1) will return negative
+        return this.name.toLowerCase().compareTo(t.name.toLowerCase());
     }
 }
