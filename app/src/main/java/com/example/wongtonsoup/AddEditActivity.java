0d8ea5f1a811195bf3caef3a7ac585275f1b4ddf
Lina Saha <linaaman@ualberta.ca>
2023-11-05 20:00:47 -0700

    Last little edits to add/edit item
    added mock code for tags to be view as a ChipGroup

--- app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
+++ app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
@@ -6,7 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.List;
+
 public class AddEditActivity extends AppCompatActivity {
+    // Assume we have item with associated tags lise
+    private Item item;
+    //List<Tag> tags = item.getTags();
+
+    // Find the ChipGroup view in layout
+    ChipGroup chipGroup = findViewById(R.id.add_edit_chip_group);
+//    //Iterate through tags and add a chip for each one
+//    for (Tag tag : tags) {
+//        Chip chip = new Chip(this);
+//        chip.setText(tag.getName());
+//        chip.setCloseIconVisible(true);
+//    //Add a click listener to handle interactions with the tag
+//    chip.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onLongClick(View view) {
+//            // Handle tag click, e.g., remove the tag from the item
+//        }
+//    });
+//
+//    // Add the Chip to the ChipGroup
+//    chipGroup.addView(chip);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
