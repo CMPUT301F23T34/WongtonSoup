dfdb691b6db6da15a2e8c3f9385dd5b5dd9aaae5
Lina Saha <116121905+lina-doughnuts@users.noreply.github.com>
2023-11-06 09:14:01 -0700

    Finished maybe add/edit, view Item, expanded search, tags dialog (#54)
    * added triangle class that extends shape
    
    * added colour to shape class
    
    * added some attributes to add_edit before my computer broke
    
    added description, serial number and half date. It broke when I tried adding a date type instead of regular text :(
    
    * Reformatted and added an icon
    
    tried to keep going where I left off but android studio broke again when I tried to add another icon
    
    * almost done add/edit just needs tags
    
    If it wasn't for android studio breaking on me I would make way less frequent commits
    
    * It broke again
    
    I lost a lot of progress on that last commit so I had to redo some stuff here but it doesn't look like all the changes I made this time will be committed either :(
    
    * add/edit UI done + my android studio updated and changed things in build.gradle.kts
    
    hopefully if everyone updates we won't have a problem ?
    
    * Last little edits to add/edit item
    
    added mock code for tags to be view as a ChipGroup
    
    * image visibility + add tag button
    
    I kept thinking of stuff as I was writing the description of the pull request tehe
    
    * finished Item View
    
    tried to make it as close to the mockup as possible
    
    * Added expanded search to activity_main.xml
    
    Added sort by and filter by options
    Scrap having to pull down to see expanded for this sprint, it will always show
    
    * added tag dialog layout
    
    I keep using Chip Group for tags and I sure hope it works. If it isn't the easiest implementation of tags we can change it and blame chat GPT :)
    
    ---------
    
    Co-authored-by: Matthew Jacobs <89949279+WindowsDotExe@users.noreply.github.com>

--- app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
+++ app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
@@ -6,13 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.List;
+
 public class AddEditActivity extends AppCompatActivity {
+    // Assume we have item with associated tags lise
+    private Item item;
+    //List<Tag> tags = item.getTags();
+
+    // Find the ChipGroup view in layout
+    ChipGroup chipGroup = findViewById(R.id.add_edit_chip_group);
+//    //Iterate through tags and add a chip for each one
+//    for (Tag tag : tags) {
+//        Chip chip = new Chip(this);
+//        chip.setText(tag.getName());
+//        chip.setCloseIconVisible(true);
+//    //Add a click listener to handle interactions with the tag
+//    chip.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onLongClick(View view) {
+//            // Handle tag click, e.g., remove the tag from the item
+//        }
+//    });
+//
+//    // Add the Chip to the ChipGroup
+//    chipGroup.addView(chip);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit);
-        Button back = findViewById(R.id.add_back_button);
+        Button back = findViewById(R.id.add_edit_back_button);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
