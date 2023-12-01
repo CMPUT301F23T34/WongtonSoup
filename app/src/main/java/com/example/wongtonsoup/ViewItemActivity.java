ee85f148e48b18220f2209c01ccfff965e5e9dca
Matthew Jacobs <mjacobs@ualberta.ca>
2023-12-01 13:46:19 -0700

    Changes for ID preservation.

--- app/src/main/java/com/example/wongtonsoup/ViewItemActivity.java
+++ app/src/main/java/com/example/wongtonsoup/ViewItemActivity.java
@@ -69,6 +69,9 @@ public class ViewItemActivity extends AppCompatActivity {
                 newIntent.putExtra("Date", dateText);
                 newIntent.putExtra("Price", priceText);
                 newIntent.putExtra("Serial", serialText);
+                String id = intent.getStringExtra("ID");
+                Log.d("ViewItemActivity", "Passing Item ID: " + id); // Log to confirm ID is received
+                newIntent.putExtra("ID", id);
                 startActivityForResult(newIntent, ADD_EDIT_REQUEST_CODE);
             }
         });
@@ -85,19 +88,36 @@ public class ViewItemActivity extends AppCompatActivity {
     private void finishAndPassItem(Item item) {
         Intent resultIntent = new Intent();
         resultIntent.putExtra("resultItem", item);
+        resultIntent.putExtra("itemID", item.getID()); // Pass the ID back
         setResult(RESULT_OK, resultIntent);
         finish();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == ADD_EDIT_REQUEST_CODE && resultCode == RESULT_OK) {
-            // Check if the request code matches and the result is OK
             if (data != null && data.hasExtra("resultItem")) {
                 Item resultItem = (Item) data.getSerializableExtra("resultItem");
+                String itemID = data.getStringExtra("itemID"); // Receive the ID back
+                Log.d("ViewItemActivity RECEIVE", "Received Item ID: " + itemID); // Log to confirm ID is received
+                resultItem.setID(itemID); // Set the ID to the resultItem
                 finishAndPassItem(resultItem);
             }
         }
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == ADD_EDIT_REQUEST_CODE && resultCode == RESULT_OK) {
+//            // Check if the request code matches and the result is OK
+//            if (data != null && data.hasExtra("resultItem")) {
+//                Item resultItem = (Item) data.getSerializableExtra("resultItem");
+//                finishAndPassItem(resultItem);
+//            }
+//        }
+//    }
 }
