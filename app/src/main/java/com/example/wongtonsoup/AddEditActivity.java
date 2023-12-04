6b9f80fb4463896f8105a74698a2332cea1dc6d2
TonyHu72 <tyhu@ualberta.ca>
2023-12-04 13:57:37 -0700

    Added unit tests

--- app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
+++ app/src/main/java/com/example/wongtonsoup/AddEditActivity.java
@@ -43,7 +43,8 @@ import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
